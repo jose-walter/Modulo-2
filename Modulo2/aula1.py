bc56e835feeb6306b5c572b58e3ea412564ab676
jose-walter <riothey@hotmail.com>
2024-10-22 11:59:54 -0300

    Add files via upload

---
+++ Modulo2/aula1.py
@@ -0,0 +1,52 @@
+# lista = [1,2,3,4,5]
+# for numero in lista:
+#     print(numero)
+
+# lista = ['infinity', 'School' , 'Recife']
+# cidade = input('Digite o nome da cidade: ')
+# lista[2]
+
+
+# lista = ['Recife', 22, False]
+# print(lista[2])
+
+
+# lista = ['infinity', 'School' , 'Recife']
+# lista.append('Fortaleza')
+# lista.insert(1, 'Salvador')
+# print(lista)
+# lista.remove('Salvador')
+# print(lista)
+# lista.pop(1)
+# print(lista)
+
+
+# frutas = ('Maça','banana','uva','laranja')
+# indice = 0
+# for fruta in frutas:
+#     if frutas[indice] == 'laranja':
+#         print(f'O índice de laranja é: {indice}')
+#     else:
+#         indice += 1
+        
+# print(f' O índice de laranja pelo método é: {frutas.index('laranja')}')
+
+
+# frutas = ('Maça','banana','uva','laranja')
+# contador = 0
+# for fruta in frutas:
+#     if fruta == 'banana':
+#         contador += 1
+# print(f'A quantidade de bananas dentro da tupla é: {contador}')        
+# print(f'A quantidade de banana pelo método é: {frutas.count('banana')}')
+
+
+# ATIVIDADE PRÁTICA 4
+
+
+# palestrante1 = ('Jose','Água doce','Compesa')
+# palestrante2 = ('Thiago','Línguas','CCAA')
+# palestrante3 = ('Ronaldo','Fé','Igreja Batista')
+
+
+# print(f'O palestrante {palestrante3[0]} vai ter o tema {palestrante3[1]} e é da instituição {palestrante3[2]}.')
