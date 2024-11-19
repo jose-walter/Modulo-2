29282dc900f82dd0740bbf3911fc59a0011e74ef
jose-walter <riothey@hotmail.com>
2024-11-19 12:02:09 -0300

    Add files via upload

---
+++ Modulo2/MOD 2/aula5.py
@@ -0,0 +1,39 @@
+# tot_notas = 0
+
+# for notas in range(3):
+#     notas = float(input('Insira as notas(UMA DE CADA VEZ): '))
+#     tot_notas += notas
+# media = tot_notas / 3
+# print(f'A média das 3 notas é {media}')
+
+# def calcular_media(n1, n2, n3):
+#     media = (n1 + n2 + n3) / 3
+#     return media
+
+# nota1 = float(input('Digite a 1° nota: '))
+# nota2 = float(input('Digite a 2° nota: '))
+# nota3 = float(input('Digite a 3° nota: '))
+# resultado = calcular_media(nota1, nota2, nota3)
+# print(f'A média das 3 notas é {resultado}')
+
+
+# def criar_lista_de_compras(*args):
+#     return list(args)
+
+# print(criar_lista_de_compras('Arroz, Feijão, Carne'))
+
+
+# numeros = [1, 2, 3, 4, 5]
+# quadrados = list(map(lambda x: x ** 2, numeros))
+# print(quadrados)
+
+
+# numeros = [1,2,3,4]
+# dobro_lambda = list(map(lambda numero: numero * 2, numeros))
+# print(dobro_lambda)
+
+
+# numeros = [1,2,3,4]
+# pares = list(filter(lambda numero: numero % 2 == 0, numeros))
+# print(pares)
+
