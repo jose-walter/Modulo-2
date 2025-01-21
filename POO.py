169625fbf8a03951eb67549e21f7f3369f7a2a89
jose-walter <riothey@hotmail.com>
2025-01-21 11:58:22 -0300

    Add files via upload

---
+++ POO.py
@@ -0,0 +1,65 @@
+# class Carro:
+#     def __init__(self, modelo, cor, ano):
+#         self.modelo = modelo
+#         self.cor = cor
+#         self.ano = ano
+#         self.celocidade = 0
+
+#     def __str__(self):
+#         return f'Modelo: {self.modelo}\nCor: {self.cor}\nAno: {self.ano}'
+    
+# fusca = Carro('Fusca', 'Verde', 1984)
+
+# print(fusca)
+
+
+# class Home_teather:
+#     def __init__(self, canais, cor, tamanho):
+#         self.canais = canais
+#         self.cor = cor
+#         self.tamanho = tamanho
+
+#     def __str__(self):
+#         return f'Quantidade de caixas: {self.canais}\nCor: {self.cor}\nTamanho: {self.tamanho}'
+    
+# lg = Home_teather('5.1', 'preto', '25x40')
+
+# print(lg)
+
+
+
+# class Operações:
+#     def __init__(self,n1, n2):
+#         self.n1 = n1
+#         self.n2 = n2
+
+#     def soma(self):
+#         resultado = self.n1 + self.n2
+#         print(f'{self.n1} + {self.n2} = {resultado}')
+    
+#     def subtrair(self):
+#         resultado = self.n1 - self.n2
+#         print(f'{self.n1} - {self.n2} = {resultado}')
+    
+#     def multiplicar(self):
+#         resultado = self.n1 * self.n2
+#         print(f'{self.n1} x {self.n2} = {resultado}')
+    
+#     def dividir(self):
+#         if self.n2 != 0:
+#             resultado = self.n1 / self.n2
+#             print(f'{self.n1} + {self.n2} = {resultado}')
+#         else:
+#             print('ERRO')
+        
+    
+# n1 = int(input('Digite o primeiro numero: '))
+# n2 = int(input('Digite o segundo numero: '))
+# teste = Operações(n1, n2)
+# teste.soma()
+# teste.subtrair()
+# teste.multiplicar()
+# teste.dividir()
+
+
+
